d5ddcaf608c0deed6eba28ece9041e5e746fe715
pr0pellerhead <bojan.gavrovski@gmail.com>
2015-03-20 13:12:44 +0100

    loops and templating

--- upload.php
+++ upload.php
@@ -1,7 +1,7 @@
 <?php 
 
 // print_r($_FILES);
-if('image/jpeg' == $_FILES['slika']['type'] && 1024 * 1024 * 1024 > $_FILES['slika']['size']){
+if('image/jpeg' == $_FILES['slika']['type'] && 1073741824 > $_FILES['slika']['size']){
 	
 	$od = $_FILES['slika']['tmp_name'];
 	$vo = 'uploads/'.$_FILES['slika']['name'];
